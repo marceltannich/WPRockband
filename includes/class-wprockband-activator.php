605e864fc7fc74ca2d79c8d81cffa635e2c936cc
Marcel T <marceltannich@gmail.com>
2019-06-25 07:40:00 +0200

    First commit

---
+++ includes/class-wprockband-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.tannich.com
+ * @since      1.0.0
+ *
+ * @package    Wprockband
+ * @subpackage Wprockband/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wprockband
+ * @subpackage Wprockband/includes
+ * @author     Marcel Tannich <marcel@tannich.com>
+ */
+class Wprockband_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wprockband-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.tannich.com
+ * @since      1.0.0
+ *
+ * @package    Wprockband
+ * @subpackage Wprockband/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wprockband
+ * @subpackage Wprockband/includes
+ * @author     Marcel Tannich <marcel@tannich.com>
+ */
+class Wprockband_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
